dc9ef4599bab643148d2da2cafc86238a1e4506e
Paul Joubert <paul@trespaul.com>
2024-04-08 16:29:17 +0200

    add musnix.

--- flake.nix
+++ flake.nix
@@ -10,34 +10,41 @@
         { url = "github:nix-community/home-manager";
           inputs.nixpkgs.follows = "nixpkgs";
         };
+      musnix =
+        { url = "github:musnix/musnix";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-  outputs = { nixpkgs, agenix, home-manager, ... }:
+  outputs = { nixpkgs, agenix, home-manager, musnix, ... }:
     { nixosConfigurations =
         let
-          mkConfig = hostname:
-              nixpkgs.lib.nixosSystem
-                { system = "x86_64-linux";
-                  modules =
-                    [ ./common/configuration.nix
-                      ./machines/${hostname}/configuration.nix
-                      agenix.nixosModules.default
-                      home-manager.nixosModules.home-manager
-                      { home-manager =
-                          { useGlobalPkgs = true;
-                            useUserPackages = true;
-                            users.paul.imports =
-                              [ ./common/home.nix
-                                ./machines/${hostname}/home.nix
-                                agenix.homeManagerModules.default
-                                ./secrets/secrets.module.nix
-                              ];
-                          };
-                      }
-                    ];
-                };
+          mkConfig = { hostname, extraModules }:
+            nixpkgs.lib.nixosSystem
+              { system = "x86_64-linux";
+                modules =
+                  [ ./common/configuration.nix
+                    ./machines/${hostname}/configuration.nix
+                    agenix.nixosModules.default
+                    home-manager.nixosModules.home-manager
+                    { home-manager =
+                        { useGlobalPkgs = true;
+                          useUserPackages = true;
+                          users.paul.imports =
+                            [ ./common/home.nix
+                              ./machines/${hostname}/home.nix
+                              agenix.homeManagerModules.default
+                              ./secrets/secrets.module.nix
+                            ];
+                        };
+                    }
+                  ] ++ extraModules;
+              };
         in
-          { "paulpad"   = mkConfig "paulpad";
+          { "paulpad" = mkConfig
+              { hostname = "paulpad";
+                extraModules = [ musnix.nixosModules.musnix ];
+              };
             "polyaenus" = mkConfig "polyaenus";
           };
     };
