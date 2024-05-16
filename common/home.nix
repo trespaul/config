b261655919eaef58a40c8dd1ae091f693bc3b7fa
Paul Joubert <paul@trespaul.com>
2024-05-16 14:16:50 +0200

    add starship.

--- common/home.nix
+++ common/home.nix
@@ -313,6 +313,106 @@
             };
         };
 
+      starship =
+        { enable = true;
+          enableBashIntegration = true;
+          enableZshIntegration = true;
+          settings =
+            { format =
+                "[█](color_orange)$shell$username$hostname[█🭛](bg:color_yellow fg:color_orange)$directory[█🭛](fg:color_yellow bg:color_aqua)$git_branch$git_status[█🭛](fg:color_aqua bg:color_blue)$rust$nodejs$haskell$python[█🭛](fg:color_blue bg:color_bg3)$docker_context[█🭛](fg:color_bg3 bg:color_bg1)$time[█🭛](fg:color_bg1)$line_break$character";
+              palette = "gruvbox_dark";
+              palettes.gruvbox_dark =
+                { color_fg0    = "#fbf1c7";
+                  color_bg0    = "#1d2021";
+                  color_bg1    = "#3c3836";
+                  color_bg3    = "#665c54";
+                  color_blue   = "#458588";
+                  color_aqua   = "#689d6a";
+                  color_green  = "#98971a";
+                  color_orange = "#d65d0e";
+                  color_purple = "#b16286";
+                  color_red    = "#cc241d";
+                  color_yellow = "#d79921";
+                };
+              os.disabled = true;
+              hostname.style = "bg:color_orange";
+              line_break.disabled = false;
+              username =
+                { style_user = "bg:color_orange fg:color_fg0";
+                  style_root = "bg:color_orange fg:color_fg0";
+                  format = "[ $user ]($style)";
+                };
+              directory =
+                { style = "fg:color_fg0 bg:color_yellow";
+                  format = "[ $path ]($style)";
+                  truncation_length = 3;
+                  truncation_symbol = "…/";
+                  substitutions =
+                    { "Documents" = "󰈙 ";
+                      "Downloads" = " ";
+                      "Music"     = "󰝚 ";
+                      "Pictures"  = " ";
+                      "Projects"  = "󰲋 ";
+                    };
+                };
+              shell =
+                { disabled = false;
+                  zsh_indicator = "";
+                  bash_indicator = "";
+                  style = "bg:color_orange";
+                  format = "[$indicator]($style)";
+                };
+              git_branch =
+                { symbol = "";
+                  style = "bg:color_aqua";
+                  format = "[[ $symbol $branch ](fg:color_fg0 bg:color_aqua)]($style)";
+                };
+              git_status =
+                { style = "bg:color_aqua";
+                  format = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_aqua)]($style)";
+                };
+              nodejs =
+                { symbol = "";
+                  style = "bg:color_blue";
+                  format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+                };
+              rust =
+                { symbol = "";
+                  style = "bg:color_blue";
+                  format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+                };
+              haskell =
+                { symbol = "";
+                  style = "bg:color_blue";
+                  format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+                };
+              python =
+                { symbol = "";
+                  style = "bg:color_blue";
+                  format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+                };
+              docker_context =
+                { symbol = "";
+                  style = "bg:color_bg3";
+                  format = "[[ $symbol( $context) ](fg:#83a598 bg:color_bg3)]($style)";
+                };
+              time =
+                { disabled = false;
+                  time_format = "%R";
+                  format = "[[  $time ](fg:color_fg0 bg:color_bg1)]($style)";
+                };
+              character =
+                { disabled = false;
+                  success_symbol = "[](bold fg:color_green)";
+                  error_symbol = "[](bold fg:color_red)";
+                  vimcmd_symbol = "[](bold fg:color_green)";
+                  vimcmd_replace_one_symbol = "[](bold fg:color_purple)";
+                  vimcmd_replace_symbol = "[](bold fg:color_purple)";
+                  vimcmd_visual_symbol = "[](bold fg:color_yellow)";
+                };
+            };
+        };
+
       firefox =
         { enable = true;
           profiles.default.userChrome =
