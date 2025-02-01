b6301a90a463f6f7706b160340333ae91eba2f72
Paul Joubert <paul@trespaul.com>
2025-02-01 14:37:47 +0200

    spotifyd: use zeroconf instead of login.

--- machines/polyaenus/home.nix
+++ machines/polyaenus/home.nix
@@ -6,12 +6,11 @@
       spotifyd =
         { enable = true;
           settings.global =
-            { username = "21wt6kx2cpvurgqvkgwk34jsy";
-              password_cmd = "cat ${config.age.secrets.spotify.path}";
-              device_name = "polyaenus";
+            { device_name = "polyaenus";
               bitrate = 320;
               cache_path = "/home/paul/.cache/spotifyd";
               device_type = "a_v_r";
+              zeroconf_port = 1234;
             };
         };
     };
