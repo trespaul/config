f4eaf81e588c15043c1030712a64d64f7c49cde3
Paul Joubert <paul@trespaul.com>
2025-02-01 15:02:03 +0200

    update paulpad apps.

--- machines/paulpad/home.nix
+++ machines/paulpad/home.nix
@@ -8,7 +8,7 @@
           ardour audacity bespokesynth calf cardinal haskellPackages.tidal
           musescore supercollider-with-sc3-plugins vital
           # graphics & video apps
-          blender darktable eyedropper gimp inkscape-with-extensions kdenlive
+          darktable eyedropper gimp hugin inkscape-with-extensions kdenlive
           scrcpy
           # dev etc.
           android-tools arduino-ide boxes cargo flyctl gleam google-cloud-sdk
@@ -18,11 +18,11 @@
           nodePackages.typescript-language-server vscode-langservers-extracted
           yaml-language-server
           # pentesting
-          aircrack-ng bettercap crunch dsniff hashcat hcxdumptool hcxtools iw
-          macchanger tcpdump wifite2 wireshark
+          aircrack-ng bettercap hashcat hcxdumptool hcxtools iw macchanger
+          tcpdump wireshark
           # internet apps
           beeper discord fractal newsflash signal-desktop telegram-desktop
-          tor-browser-bundle-bin transmission_4-gtk tuba wike
+          transmission_4-gtk tuba wike
           # document cli utils
           ocrmypdf texliveFull librsvg
           # document apps
@@ -37,14 +37,5 @@
 
   programs =
     { obs-studio.enable = true;
-      ncspot =
-        { enable = true;
-          # use_nerdfont = true;
-          # notify = true;
-          # credentials =
-          #   { username = "21wt6kx2cpvurgqvkgwk34jsy";
-          #     password_cmd = "cat ${config.age.secrets.spotify.path}";
-          #   };
-        };
     };
 }
