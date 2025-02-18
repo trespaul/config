35354f200041a1a110a9a29ae6cc1cd319b5d16f
Paul Joubert <paul@trespaul.com>
2025-02-18 20:00:43 +0200

    add setting for internet sharing on polyaenus.

--- machines/polyaenus/configuration.nix
+++ machines/polyaenus/configuration.nix
@@ -15,10 +15,57 @@
               5353        # spotifyd zeroconf
             ];
         };
+      interfaces."enp3s0" = # internet sharing
+        { useDHCP = false;
+          ipv4.addresses =
+            [ { address = "10.0.0.1"; prefixLength = 24; } ];
+        };
+      nftables =
+        { ruleset = # internet sharing
+            ''
+              table ip nat {
+                chain POSTROUTING {
+                  type nat hook postrouting priority 100;
+                  oifname "wlp2s0" counter masquerade
+                }
+              }
+              table ip filter {
+                chain INPUT {
+                  iifname "enp3s0" counter accept
+                }
+              }
+            '';
+        };
     };
 
   services =
     {
+      kea.dhcp4 = # internet sharing
+        { enable = true;
+          settings =
+            { valid-lifetime = 4000;
+              renew-timer = 1000;
+              rebind-timer = 2000;
+              interfaces-config.interfaces = [ "enp3s0" ];
+              subnet4 =
+                [ { id = 1;
+                    subnet = "10.0.0.1/24";
+                    pools = [ { pool = "10.0.0.2 - 10.0.0.255"; } ];
+                  }
+                ];
+              lease-database =
+                { type = "memfile";
+                  name = "/var/lib/kea/dhcp4.leases";
+                  persist = true;
+                };
+              option-data =
+                [ { name = "routers";
+                    data = "10.0.0.1";
+                  }
+                ];
+            };
+        };
+
       openssh =
         { enable = true;
           settings =
