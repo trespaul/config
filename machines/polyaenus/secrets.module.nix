5925e364980e085c55ba23770145d191f6ee4754
Paul Joubert <paul@trespaul.com>
2025-02-23 13:47:25 +0200

    update and reorganise secrets.
    - add new machines' ssh keys
    - move age files to subfolder

--- machines/polyaenus/secrets.module.nix
+++ machines/polyaenus/secrets.module.nix
@@ -1,18 +1,10 @@
 {
-  age =
-    { identityPaths =
-        [ "/home/paul/.ssh/id_ed25519"
-          "/etc/ssh/ssh_host_ed25519_key"
-        ];
-      secrets =
-        { cloudflare-tunnel =
-            { file = ../../secrets/cloudflare-tunnel.age;
-              owner = "cloudflared";
-              mode = "600";
-            };
-          anmari-cms =
-            { file = ../../secrets/container_anmari-cms_config.age;
-            };
-        };
+  age.secrets.cloudflare-tunnel =
+    { file = ../../secrets/encrypted/cloudflare-tunnel.age;
+      owner = "cloudflared";
+      mode = "600";
+    };
+  age.secrets.anmari-cms =
+    { file = ../../secrets/encrypted/container_anmari-cms_config.age;
     };
 }
