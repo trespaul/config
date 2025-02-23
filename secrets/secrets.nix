fe168345dac02de1286276c2d1c3a780a253d2b6
Paul Joubert <paul@trespaul.com>
2025-02-23 15:11:09 +0200

    re-add dionysius.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -15,7 +15,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6le6hCVJGJE9ScupP6jq8ab2BntFBFoUQGUvgpYXL2";
 
   dionysius =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXPB6LC4/qZt+OS8bkd2SUYnLK3ua/lnW1aXKElgOBN";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACqFS1APLey6k/gRe/HOmyt87BqhpwJazHFiZmDKqJb";
 
   all = [ paulpad polyaenus metrodorus leontion hermarchus dionysius ];
 in
