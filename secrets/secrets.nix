3a1135576c91d9a8502d79ebad104c37fb101bb9
Paul Joubert <paul@trespaul.com>
2025-02-23 13:47:25 +0200

    update and reorganise secrets.
    - add new machines' ssh keys
    - move age files to subfolder

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -8,10 +8,18 @@ let
   metrodorus =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXoAsnGMn7WqPeVZ2KYeghyl4Fb6Ho9nHTxVU9jGBj4";
 
-  all = [ paulpad polyaenus metrodorus ];
+  leontion =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhucnrXXBvpgafmk1eJ+DUaTRnNldiAdxm1d7+Xzclv";
+
+  hermarchus =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6le6hCVJGJE9ScupP6jq8ab2BntFBFoUQGUvgpYXL2";
+
+  dionysius =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXPB6LC4/qZt+OS8bkd2SUYnLK3ua/lnW1aXKElgOBN";
+
+  all = [ paulpad polyaenus metrodorus leontion hermarchus dionysius ];
 in
   {
-    "spotify.age".publicKeys = all;
-    "cloudflare-tunnel.age".publicKeys = [ polyaenus ];
-    "container_anmari-cms_config.age".publicKeys = [ paulpad polyaenus ];
+    "encrypted/cloudflare-tunnel.age".publicKeys = [ polyaenus ];
+    "encrypted/container_anmari-cms_config.age".publicKeys = [ polyaenus paulpad ];
   }
