8bb79f37ae738e50c67b6d5782241452405deb37
Paul Joubert <paul@trespaul.com>
2025-05-12 14:30:16 +0200

    metrodorus: add jellyfin.

--- machines/metrodorus/configuration.nix
+++ machines/metrodorus/configuration.nix
@@ -9,8 +9,47 @@
         { enable = true;
           interval = "weekly";
         };
+      caddy =
+        { enable = true;
+          globalConfig = "skip_install_trust";
+          virtualHosts =
+            { "jf.local".extraConfig =
+                ''
+                  reverse_proxy http://localhost:8096
+                  tls internal
+                '';
+            };
+        };
+
+      jellyfin.enable = true;
           settings =
             };
         };
+    };
+
+
+  environment.systemPackages = with pkgs;
+    [ jellyfin jellyfin-web jellyfin-ffmpeg ];
+
+  # video hardware acceleration
+
+  nixpkgs.config.packageOverrides = pkgs:
+    { intel-vaapi-driver =
+        pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+    };
+
+  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "i965";
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "i965";
+
+  hardware.graphics =
+    { enable = true;
+      extraPackages = with pkgs;
+        [ intel-vaapi-driver
+          libva-vdpau-driver
+          # intel-compute-runtime
+          # intel-compute-runtime-legacy1
+          intel-media-sdk
+          intel-ocl
+        ];
     };
 }
