620f38bcdf934cc1957f053b9319cb2525ecbd6c
Paul Joubert <paul@trespaul.com>
2025-05-12 13:00:26 +0200

    re-add metrodorus.

--- machines/metrodorus/hardware-configuration.nix
+++ machines/metrodorus/hardware-configuration.nix
@@ -10,9 +10,6 @@
         { kernelModules = [ ];
           availableKernelModules =
             [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
-          luks.devices."luks-610e95b4-a88a-4da3-b7ab-692e1cf2b4e0".device =
-            "/dev/disk/by-uuid/610e95b4-a88a-4da3-b7ab-692e1cf2b4e0";
-          secrets."/crypto_keyfile.bin" = null;
         };
       kernelModules = [ "kvm-intel" ];
       extraModulePackages = [ ];
@@ -20,12 +17,21 @@
 
   fileSystems =
     { "/" =
-        { device = "/dev/disk/by-uuid/5931abbf-10a5-4177-b33e-cc73c361dc31";
+        { device = "/dev/disk/by-uuid/1295414d-7b39-44ec-afff-70b7f59da36c";
           fsType = "ext4";
         };
       "/boot" =
-        { device = "/dev/disk/by-uuid/31DE-3341";
+        { device = "/dev/disk/by-uuid/1681-87EF";
           fsType = "vfat";
+        };
+      "/mnt/Storage" =
+        { device = "/dev/disk/by-uuid/5598210a-03ea-4626-937f-7e70950e3533";
+          fsType = "btrfs";
+          options = [ "compress=zstd" ];
+        };
+      "/mnt/Media" =
+        { device = "/dev/disk/by-uuid/a7cca8b2-0cc7-49f0-a292-67e534b41ca0";
+          fsType = "ext4";
         };
     };
 
