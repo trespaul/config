8c9f0f7923e962da04319ce4a1293eaea61ca525
Paul Joubert <paul@trespaul.com>
2025-05-12 14:38:56 +0200

    formatting.

--- machines/paulpad/configuration.nix
+++ machines/paulpad/configuration.nix
@@ -17,9 +17,7 @@
     { extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
       kernelModules = [ "v4l2loopback" ];
       extraModprobeConfig =
-        ''
-          options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-        '';
+        ''options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"'';
     };
 
   musnix.enable = true;
