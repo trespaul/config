d22966495fbd6c1b2791178c401ab3298c6621f4
Paul Joubert <paul@trespaul.com>
2025-06-27 14:35:39 +0200

    polyaenus: update directus container.

--- machines/polyaenus/configuration.nix
+++ machines/polyaenus/configuration.nix
@@ -130,7 +130,7 @@
       volumesDir = "/home/paul/container_volumes";
     in
       { anmari-cms =
-          { image = "directus/directus:11.8.0";
+          { image = "directus/directus:11.9.0";
             autoStart = true;
             ports = [ "8080:8055" ];
             volumes =
