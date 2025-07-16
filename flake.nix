ff14f4e7603850da668db6477687e9da251dfcfa
Paul Joubert <paul@trespaul.com>
2025-07-16 13:43:53 +0200

    flake: switch to updated zen-browser-flake.

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           inputs.nixpkgs.follows = "nixpkgs";
         };
       zen-browser =
-        { url = "github:tc-001/zen-browser-flake";
+        { url = "github:mrcjkb/zen-browser-flake";
           inputs.nixpkgs.follows = "nixpkgs";
         };
     };
