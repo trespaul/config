fc89de53c13e1f352c609979ff4572d23eae7d51
Paul Joubert <paul@trespaul.com>
2025-08-02 22:44:31 +0200

    flake: use lix main.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
 
   inputs =
     { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      lix =
+        { url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+          flake = false;
+        };
       lix-module =
-        { url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+        { url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
           inputs.nixpkgs.follows = "nixpkgs";
+          inputs.lix.follows = "lix";
         };
       ragenix =
         { url = "github:yaxitech/ragenix";
