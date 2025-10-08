d6aeff8466492e5352c21c5303496ac5df47f021
Paul Joubert <paul@trespaul.com>
2025-10-08 16:30:17 +0200

    use lix from nixpkgs, make default.
    using nixpkgs cache means all machines can use lix.

--- modules/lix.nix
+++ modules/lix.nix
@@ -1,10 +1,32 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   options.custom.lix = lib.mkOption
     { type = lib.types.bool;
-      default = false;
+      default = true;
       description = "use lix instead of cppnix.";
     };
   
-  config.lix.enable = config.custom.lix;
+  config =
+    { nixpkgs.overlays = lib.mkIf config.custom.lix
+        [ ( final: prev:
+              { inherit (prev.lixPackageSets.git)
+                  nixpkgs-review
+                  nix-eval-jobs
+                  nix-fast-build
+                  colmena;
+              }
+          )
+        ];
+
+      nix.package =
+        let
+          mkIfElse = condition: yes: no: lib.mkMerge
+            [ ( lib.mkIf condition yes )
+              ( lib.mkIf (!condition) no )
+            ];
+        in
+          mkIfElse config.custom.lix
+            pkgs.lixPackageSets.git.lix
+            pkgs.nix;
+    };
 }
