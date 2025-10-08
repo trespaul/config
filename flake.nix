5ca8154f2fd33fa552f3fbf85265e8a7d53b75d0
Paul Joubert <paul@trespaul.com>
2025-10-08 16:30:17 +0200

    use lix from nixpkgs, make default.
    using nixpkgs cache means all machines can use lix.

--- flake.nix
+++ flake.nix
@@ -3,15 +3,6 @@
   inputs =
     { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
       import-tree.url = "github:vic/import-tree";
-      lix =
-        { url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-          flake = false;
-        };
-      lix-module =
-        { url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-          inputs.nixpkgs.follows = "nixpkgs";
-          inputs.lix.follows = "lix";
-        };
       ragenix =
         { url = "github:yaxitech/ragenix";
           inputs.nixpkgs.follows = "nixpkgs";
@@ -49,7 +40,6 @@
                   ( inputs.import-tree ./machines/${hostname} )
                   { inherit custom; }
                   # external:
-                  inputs.lix-module.nixosModules.default
                   inputs.ragenix.nixosModules.default
                   inputs.musnix.nixosModules.musnix
                   inputs.home-manager.nixosModules.home-manager
@@ -67,9 +57,7 @@
           builtins.mapAttrs mkConfig
             # { hostname = custom };
             { paulpad =
-                { headless = false;
-                  lix = true;
-                };
+                { headless = false; };
               polyaenus =
                 { internet-sharing.enable = true;
                   k3s = true;
